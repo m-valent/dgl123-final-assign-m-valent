0741249878bc5ec400a50e9365f84028ace6932c
Misko Valent <55299389+m-valent@users.noreply.github.com>
2021-12-07 13:32:14 -0800

    Setup folders and files

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+Title:      Final Assignment
+
+Student:    Misko Valent
+Due Date:   Dec 08, 2021
+Filename:   index.php
+
+Class:      DGL 123 - Introduction to PHP
+Semester:   2021 Fall North Island College
+Instructor: Brad Best
+-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link
+      rel="shortcut icon"
+      href="images/cards/joker_red.png"
+      type="image/x-icon"
+    /> <!-- intention is to alter the image at end of trick -->
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+
+    <title>Your Card by Misko Valent</title>
+</head>
+<body>
+    <h1>hello</h1>
+
+
+    <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+     <!-- in case I feel to style the page, 
+        if need be bootstrap 4.6 is something I am used to -->
+</body>
+</html>
